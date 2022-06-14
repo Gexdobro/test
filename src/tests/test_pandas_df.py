6519beae36be84df8bf359a21ccf5885f1dc1602
Gexdobro <k.dobrowolski1990@gmail.com>
2022-06-14 10:05:50 +0200

    new comment

--- src/tests/test_pandas_df.py
+++ src/tests/test_pandas_df.py
@@ -17,5 +17,7 @@ class TestPandasDf(unittest.TestCase):
         # assert
         assert_frame_equal(expected_df, actual_df)
 
+# this is my comment
+
 if __name__ == "__main__":
     unittest.main()
